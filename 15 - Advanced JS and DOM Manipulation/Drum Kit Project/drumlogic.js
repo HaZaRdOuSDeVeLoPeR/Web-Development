eadacc0f71f5a5fb50888285a7aa65d2894de75f
HaZaRdOuSDeVeLoPeR <hazardrousgamer26012003@gmail.com>
2025-10-05 04:43:51 +0530

    Starting jQuery

--- 15 - Advanced JS and DOM Manipulation/Drum Kit Project/drumlogic.js
+++ 15 - Advanced JS and DOM Manipulation/Drum Kit Project/drumlogic.js
@@ -1,33 +1,39 @@
 // select all the button objects (in same order which is defined in HTML document)
 let buttons = document.querySelectorAll("button");
 
-// add click event listeners to all buttons
-for(let i=0; i<buttons.length; i++)
-{
-    buttons[i].addEventListener(
-        "click", 
-        async function() 
+// generic function to add generic event listener to generic object
+function addListener(object, event_type){
+    object.addEventListener(event_type, async function() 
         {
-            buttons[i].classList.add("button-active");
-            let audio = new Audio("./sounds/"+buttons[i].textContent.toLowerCase()+".mp3");
+            object.classList.add("button-active");
+            let audio = new Audio("./sounds/"+object.textContent.toLowerCase()+".mp3");
             audio.play();
             setTimeout(
                 () => {
-                    buttons[i].classList.remove("button-active");
+                    object.classList.remove("button-active");
                 }, 100
             )
         }
     );
 }
 
+// add click and touch event listeners to all buttons
+for(const button of buttons)
+{
+    // can also use morder version "pointerdown" event which works on both desktop and mobile devices [pointerdown = mousedown + touchstart + click]
+    addListener(button, "click");
+    addListener(button, "touchstart");
+}
+
 // keypress lookup map
 const keyMap = {"w":0, "a":1, "s":2, "d":3, "j":4, "k":5, "l":6}
 
 // add keyboard keypress event listener to the whole document
-document.addEventListener("keypress", async function(event) {
+document.addEventListener("keypress", async function(event) 
+{
     let keypressed = event.key;
     let idx = keyMap[keypressed];
-    
+
     if(idx != undefined)
     {
         buttons[idx].classList.add("button-active");
@@ -37,5 +43,4 @@ document.addEventListener("keypress", async function(event) {
             buttons[idx].classList.remove("button-active");
         }, 100)
     }
-
 });
